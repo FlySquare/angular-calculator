dd9400da8b5dd3c68a4161f23da6c88e0fe6dd91
Umut Can arda <umutkonurinso@gmail.com>
2022-08-24 21:34:40 +0300

    added design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-calculator';
+  text = '0';
+  processOne: any = 0;
+  processTwo: any = 0;
+  lastOperation: string ;
+  itIsPositive = true;
+
+  numbers(number: any) {
+    if(this.processOne === undefined) {
+      this.text = this.text !== '0' ? this.text + '' + number : number;
+    }else{
+      this.text = number;
+      this.processTwo = this.text;
+    }
+  }
+
+  changePositive() {
+    if (this.itIsPositive) {
+      this.itIsPositive = false;
+      this.text = '-' + this.text;
+    } else {
+      this.itIsPositive = true;
+      this.text = this.text.slice(1);
+    }
+  }
+
+  plus() {
+    alert('plus');
+  }
+
+  minus() {
+    alert('minus');
+  }
+
+  multiply() {
+    alert('multiply');
+  }
+
+  divide() {
+    alert('divide');
+  }
+
+  percent() {
+    alert('percent');
+    this.processOne = Number(this.text)/100;
+    this.text = this.processOne.toString();
+  }
+
+  total() {
+    alert('total');
+  }
+
+  clear() {
+    this.text = '0';
+    this.itIsPositive = true;
+  }
 }
